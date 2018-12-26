1bebe113d46903fe0137a9877e95171852a883a6
Daniel <Danielsvens@hotmail.com>
2018-12-26 20:21:27 +0100

    test data

--- test_data/add_test_data.py
+++ test_data/add_test_data.py
@@ -22,7 +22,7 @@ radio_p1 = Radio(endpoint='http://sverigesradio.se/topsy/direkt/srapi/132.mp3',
 radio_p2 = Radio(endpoint='http://sverigesradio.se/topsy/direkt/srapi/163.mp3',
                  station='Sveriges Radio p2',
                  short='P2',
-                 active_station=1,
+                 active_station=0,
                  image='https://static-cdn.sr.se/sida/images/2562/3679054_1400_1400.jpg?preset=api-default-square',
                  last_modified=datetime.now()
                  )
@@ -32,6 +32,14 @@ radio_p3 = Radio(endpoint='http://sverigesradio.se/topsy/direkt/srapi/164.mp3',
                  short='P3',
                  active_station=0,
                  image='https://static-cdn.sr.se/sida/images/164/2186756_512_512.jpg?preset=api-default-square',
+                 last_modified=datetime.now()
+                 )
+
+classic = Radio(endpoint='https://cdn-web.tunein.com/assets/media/blank.mp3',
+                 station='Sveriges Radio p3',
+                 short='P10Classic',
+                 active_station=1,
+                 image='https://cdn-radiotime-logos.tunein.com/s130799q.png',
                  last_modified=datetime.now()
                  )
 
